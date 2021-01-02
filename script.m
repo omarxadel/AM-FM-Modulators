b809c9f44699d478509284c7575f45ce6d225614
Omar Adel <48330931+omarxadel@users.noreply.github.com>
2021-01-02 19:14:33 +0200

    Comment script.m file
    Comment script.m file to create separate partitions for collaborators to code easily

---
+++ script.m
@@ -0,0 +1,17 @@
+% 1.AM Modulation
+
+% 1.1.Reading the audio input.
+
+% 1.2.Filtering frequencies > 4KHz.
+
+% 1.3.Filtered signal in time domain.
+
+% 1.4.Playing the filtered audio signal (sound).
+
+% 1.5.Modulating the signal.
+
+% 1.6.Envelope detector demodulation.
+
+% 1.7.Sketching and playing the recieved signal.
+
+% 1.8.Adding noise then demodulating the signal.
